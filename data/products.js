90564e2dc5787fee204deb7afd8bdd146d002cd6
Irfanizam <irfanizam34@gmail.com>
2023-12-30 22:40:16 +0800

    update add js to receipt.html and print order into json

--- data/products.js
+++ data/products.js
@@ -4,41 +4,41 @@ export const productsData = {
     {
       itemId: "1",
       itemName: "Product A",
-      itemPrice: 9.8,
+      price: 9.8,
     },
     {
       itemId: "2",
       itemName: "Product B",
-      itemPrice: 54.5,
+      price: 54.5,
     },
     {
       itemId: "3",
       itemName: "Product C",
-      itemPrice: 20.0,
+      price: 20.0,
     },
     {
       itemId: "4",
       itemName: "Product D",
-      itemPrice: 35.3,
+      price: 35.3,
     },
     {
       itemId: "5",
       itemName: "Product E",
-      itemPrice: 12.3,
+      price: 12.3,
     },
   ],
   drinks: [
     {
       itemId: "1",
       itemName: "air",
-      itemPrice: 9.8,
+      price: 9.8,
     },
   ],
   desserts: [
     {
       itemId: "1",
       itemName: "cake",
-      itemPrice: 9.8,
+      price: 9.8,
     },
   ],
   // Add more categories as needed
---
+++ scripts/receipt.js
@@ -0,0 +1,100 @@
+// Function to generate a unique order ID
+function generateOrderId() {
+  return "ORD" + Date.now().toString(36).toUpperCase();
+}
+
+// Function to get the current date and time
+function getCurrentDateTime() {
+  const now = new Date();
+  return now.toISOString();
+}
+
+function handleFormSubmission() {
+    const customerName = document.getElementById("customer-name").value;
+    const phoneNumber = document.getElementById("cust-phone").value;
+    const address = document.getElementById("cust-address").value;
+  
+    // Generate unique order ID and get current date and time
+    const orderId = generateOrderId();
+    const orderDate = getCurrentDateTime();
+  
+    // Combine customer information with order details
+    const newOrder = {
+      orderId,
+      orderDate,
+      customer: {
+        customerName,
+        phoneNumber,
+        address,
+      },
+      items: orderDetails.cartItems,
+      totalPrice: orderDetails.totalPrice,
+    };
+  
+    // Convert the JavaScript object to JSON
+    const jsonContent = JSON.stringify(newOrder, null, 2);
+  
+    // Create a Blob with the JSON content
+    const blob = new Blob([jsonContent], { type: "application/json" });
+  
+    // Create a link element
+    const link = document.createElement("a");
+  
+    // Set the href attribute to a data URL representing the Blob
+    link.href = URL.createObjectURL(blob);
+  
+    // Set the download attribute to specify the filename
+    link.download = "order.json";
+  
+    // Append the link to the document
+    document.body.appendChild(link);
+  
+    // Trigger a click event on the link to start the download
+    link.click();
+  
+    // Remove the link from the document
+    document.body.removeChild(link);
+  
+    // Optionally, you can redirect the user to a thank you page or do other actions
+    alert("Order completed. Thank you!");
+  }
+  
+  
+
+// Call the handleFormSubmission function when the "Finish" button is clicked
+const finishBtn = document.getElementById("finish-btn");
+finishBtn.addEventListener("click", handleFormSubmission);
+
+// Function to display the order details on the receipt page
+function displayOrderDetails(orderDetails) {
+  const receiptContainer = document.getElementById("receipt-container");
+  receiptContainer.innerHTML = "<h2>Your Order Details:</h2>";
+
+  if (orderDetails && orderDetails.cartItems.length > 0) {
+    const ul = document.createElement("ul");
+
+    orderDetails.cartItems.forEach((item) => {
+      const li = document.createElement("li");
+      li.textContent = `${item.itemName} - $${item.price.toFixed(2)} x ${
+        item.quantity
+      } = $${item.total.toFixed(2)}`;
+      ul.appendChild(li);
+    });
+
+    receiptContainer.appendChild(ul);
+
+    // Display total price
+    receiptContainer.innerHTML += `<p>Total Price: $${orderDetails.totalPrice}</p>`;
+  } else {
+    receiptContainer.innerHTML += "<p>No items in the order.</p>";
+  }
+}
+
+// Retrieve orderDetails from the query parameter in the URL
+const searchParams = new URLSearchParams(window.location.search);
+const orderDetailsParam = searchParams.get("orderDetails");
+const orderDetails = JSON.parse(orderDetailsParam);
+
+// Call the displayOrderDetails function
+displayOrderDetails(orderDetails);
+console.log(orderDetails);
